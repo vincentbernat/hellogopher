e1fd0aa202dcc0de6ac8351dd5e97bef56f5ee49
Vincent Bernat <vincent@bernat.ch>
2025-02-16 10:48:54 +0100

    nix: use Go 1.24 for now

--- flake.nix
+++ flake.nix
@@ -10,10 +10,10 @@
         pkgs = nixpkgs.legacyPackages."${system}";
       in
       {
-        packages.default = pkgs.buildGoModule {
+        packages.default = pkgs.buildGo124Module {
           name = "hellogopher";
           src = ./.;
-          vendorHash = "sha256-vttI+PEmx72vqWeSQo+V2J5hPsZoLu5GksJI3m02Mgg=";
+          vendorHash = "sha256-H94VW02vcAj1vUqbIH+Wq3a/KRm6b0NPRb1SJLDf9gg=";
           tags = ["release"];
           env.CGO_ENABLED = "0";
         };
